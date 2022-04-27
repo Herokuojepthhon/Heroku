b0a188eedbad3b77ae2df040ebdf4512c753573c
Herokuojepthhon <104528238+Herokuojepthhon@users.noreply.github.com>
2022-04-27 23:06:15 +0300

    Update script.py

--- script.py
+++ script.py
@@ -4,24 +4,22 @@ class Script(object):
 
 I am Heroku Managing Bot!
 
-📣 <code>You  Can Manage Your Heroku Account By This Bot</code>🤓
-
+📣 <code> ‹ يمكنك ادارة حساب هيروكو الخاص بك عن طريق ›
 💡 click Help Button for More Info
-"""
+"""‹ انقر فوق زر التعليمات للحصول على مزيد من المعلومات ›
 
     HELP_STRINGS = """
-🙋 Hey *{}*!!
+🙋 ‹اهلا بك › *{}*!!
 
-┈•••✿ 𝐇𝐄𝐋𝐏 𝐃𝐄𝐓𝐀𝐈𝐋𝐒 ✿•••┈
+┈•••✿ ‹تفاصيل المساعده› ✿•••┈
 
-1. Press /auth and open the link given by bot
+1. اضغط المصادقه وافتح الرابط الذي قدمه البوت
+2. انسخ كود ايبي ky
 
-2. Copy your Heroku account API KEY
+3. اكتب امر توكن وي التوكن مثال
+/token التوكن مالتك
 
-3. Now send your token with /token command
-
-4. You Can See Your Account By Hitting /account
-"""
+4. يمكن روية حسابك
 
     DONATE_STRING = """
 Hey Dear *{}*, Glad To Hear You Want To Donate!
@@ -35,21 +33,19 @@ There are two ways of paying him; [PayPal](paypal.me/jinspalakkattu)."""
 
 ┈•••✿ 𝐌𝐘 𝐃𝐄𝐓𝐀𝐈𝐋𝐒 ✿•••┈
 
-📢 𝐌𝐘 𝐍𝐀𝐌𝐄: 🏷️ [Heroku](https://t.me/HerokuUfsBot) 🏷️ 
+📢الاسم: 🏷️ [Heroku](https://t.me/Herokuojepthonbot) 🏷️ 
 
-👩‍💻 𝐃𝐄𝐕: 👲 [Anonymous](https://t.me/UFSChatBot)
+👩‍💻 المطور: 👲 [Anonymous](https://t.me/jepthon)
 
-💡 𝐂𝐑𝐄𝐃𝐈𝐓𝐒: Everyone in this journey
+💻 الغة التي تم صنع بها البوت: 📒 [Python 3](https://www.python.org/)
 
-💻 𝐋𝐀𝐍𝐆𝐔𝐀𝐆𝐄: 📒 [Python 3](https://www.python.org/)
+📋 المكتبه: [Pyrogram](https://docs.pyrogram.org/)
 
-📋 𝐋𝐈𝐁𝐑𝐀𝐑𝐘: [Pyrogram](https://docs.pyrogram.org/)
+🏷️ قناة البوت : 📢 [UFS BOTS](https://t.me/jepthon)
 
-🏷️ 𝐁𝐎𝐓 𝐂𝐇𝐀𝐍𝐍𝐄𝐋: 📢 [UFS BOTS](https://t.me/joinchat/7qlEga5lO0o2MTg0)
+🌐 السيرفر: [HEROKU](https://www.heroku.com/)
 
-🌐 𝐒𝐄𝐑𝐕𝐄𝐑: [HEROKU](https://www.heroku.com/)
-
-👥 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐆𝐑𝐎𝐔𝐏 : [SUPPORT GROUP](https://t.me/joinchat/6YRhp5LyjXNkNGY0)
+👥 كروب الدعم: [SUPPORT GROUP](https://t.me/JepthonSupport)
 """
 
     AUTH_STRING = """
@@ -60,8 +56,8 @@ Open the given link and create the token and paste it here in the format
 /token xxxxxxxxxxxxxxxx...
 """
 
-    INFO_CHAT_TYPES = """𝙉𝙤𝙬 𝙔𝙤𝙪 𝘾𝙖𝙣 𝙀𝙣𝙩𝙚𝙧 𝙏𝙝𝙚 𝙉𝙚𝙬 𝘼𝙥𝙥 𝙉𝙖𝙢𝙚"""
+    INFO_CHAT_TYPES = """"يمكنك الان الدخول الى التطبيق"""
 
-    ASK_RENAME = "𝙎𝙚𝙣𝙙 𝙔𝙤𝙪𝙧 𝙉𝙚𝙬 𝙉𝙖𝙢𝙚 𝙁𝙤𝙡𝙡𝙤𝙬𝙚𝙙 𝘽𝙮 '𝙧𝙚𝙣𝙖𝙢𝙚'....."
-    WAIT_MSG = "𝙋𝙡𝙚𝙖𝙨𝙚 𝙬𝙖𝙞𝙩..𝙖 𝙨𝙚𝙘𝙤𝙣𝙙"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File Thumbnail Saved. This Image Will Be Used In The Log File."
+    ASK_RENAME = "أرسل اسمك الجديد متبوعًا بـواسطه 'اعاده تسميه'....."
+    WAIT_MSG =" ثواني ويصير "
+    SAVED_CUSTOM_THUMB_NAIL = " تم حفظ الصورة المصغرة للملف المخصص.  سيتم استخدام هذه الصورة في ملف السجل.."
